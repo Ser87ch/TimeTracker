a359574ddada7c5a43e79ac03afb2a727f5432e7
Ser <Chernobrivenko@tehnosk.ru>
2013-01-17 13:07:57 +0400

    End 7 ch

--- src/ru/chernobrivenko/timetracker/MainActivity.java
+++ src/ru/chernobrivenko/timetracker/MainActivity.java
@@ -3,13 +3,18 @@ package ru.chernobrivenko.timetracker;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	TimeTrackerAdapter timeTrackerAdapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ListView lW = (ListView) findViewById(R.id.times_list);
+		timeTrackerAdapter = new TimeTrackerAdapter();
+		lW.setAdapter(timeTrackerAdapter);
 	}
 
 	@Override
